fix formula for new phenotype

strategies/mutation.py
@@ -139,7 +139,7 @@ class DirectionalMutation(UnifiedMutations):
                 mask, np.random.normal(loc=0.0, scale=self.xi, size=old_phenotype.size), np.zeros_like(old_phenotype)
             )
 
-            new_phenotype = old_phenotype + isotropic_component + self.b * directional_component
+            new_phenotype = old_phenotype + (1 - self.b) * isotropic_component + self.b * directional_component
 
             individual.set_phenotype(new_phenotype)
 